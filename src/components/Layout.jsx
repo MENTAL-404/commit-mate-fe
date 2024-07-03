820bc36826fb814d393feab92f8ff3b800b3eb11
moolmin <zzmzzm0065@gmail.com>
2024-07-03 23:07:23 +0900

    feature box shadow to todo list

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
-import SideBar from './SideBar'
-import RightSideBar from './RightSideBar'
-import styles from '../styles/Layout.module.css'
+import SideBar from './SideBar';
+import RightSideBar from './RightSideBar';
+import styles from '../styles/Layout.module.css';
 
 export default function Layout({ children }) {
   return (
     <div className={styles.main}>
       <SideBar />
-      <div className={styles.chilren}>{children}</div>
-      <RightSideBar />
+      <div className={styles.children}>{children}</div>
+      <RightSideBar className={styles.rightSideBar} />
     </div>
-  )
+  );
 }
