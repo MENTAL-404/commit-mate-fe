f0cd8fbd3cbe4de88d06cdd45a38b0188adabe31
erica0321 <hyunahrho29@gmail.com>
2024-07-05 14:07:52 +0900

    add: mockup data

---
+++ src/data/issueData.js
@@ -0,0 +1,68 @@
+const issues = {
+  data: [
+    {
+      title: '[버그] 스크롤 오류',
+      assignees: [
+        { profile_image: 'http://~' },
+        { profile_image: 'http://~~' },
+      ],
+      url: 'http://www.github.com/asdadasd/~',
+      type: 'open',
+    },
+    {
+      title: '[버그] 스크롤 오류',
+      assignees: [
+        { profile_image: 'http://~' },
+        { profile_image: 'http://~~' },
+      ],
+      url: 'http://www.github.com/asdadasd/~',
+      type: 'open',
+    },
+    {
+      title: '[버그] 스크롤 오류',
+      assignees: [
+        { profile_image: 'http://~' },
+        { profile_image: 'http://~~' },
+      ],
+      url: 'http://www.github.com/asdadasd/~',
+      type: 'open',
+    },
+    {
+      title: '[버그] 스크롤 오류',
+      assignees: [
+        { profile_image: 'http://~' },
+        { profile_image: 'http://~~' },
+      ],
+      url: 'http://www.github.com/asdadasd/~',
+      type: 'closed',
+    },
+    {
+      title: '[버그] 스크롤 오류',
+      assignees: [
+        { profile_image: 'http://~' },
+        { profile_image: 'http://~~' },
+      ],
+      url: 'http://www.github.com/asdadasd/~',
+      type: 'closed',
+    },
+    {
+      title: '[버그] 스크롤 오류',
+      assignees: [
+        { profile_image: 'http://~' },
+        { profile_image: 'http://~~' },
+      ],
+      url: 'http://www.github.com/asdadasd/~',
+      type: 'closed',
+    },
+  ],
+}
+
+const assignees = {
+  data: [
+    { nickname: 'erica', profile_image: 'http://~', open: 10, closed: 20 },
+    { nickname: 'silvia', profile_image: 'http://~', open: 10, closed: 20 },
+    { nickname: 'ian', profile_image: 'http://~', open: 10, closed: 20 },
+    { nickname: 'jun', profile_image: 'http://~', open: 10, closed: 20 },
+    { nickname: 'hong', profile_image: 'http://~', open: 10, closed: 20 },
+  ],
+}
