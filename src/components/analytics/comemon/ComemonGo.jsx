35653afb08d108e1a4e667069838c20b81e0a96c
moolmin <zzmzzm0065@gmail.com>
2024-07-09 16:37:09 +0900

    feat: connect data with comemon items

--- src/components/analytics/comemon/ComemonGo.jsx
+++ src/components/analytics/comemon/ComemonGo.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import styles from '../../../styles/ComemonGo.module.css'
 import Comemons from './Comemons'
 
+const data = [
+  { "nickname": "hong", "commit_count": 2 },
+  { "nickname": "ian", "commit_count": 80 },
+  { "nickname": "silvia", "commit_count": 30 },
+  { "nickname": "jun", "commit_count": 200 },
+  { "nickname": "erica", "commit_count": 70 }
+];
+
 export default function ComemonGo() {
   return (
     <div className={styles.container}>
@@ -9,11 +17,9 @@ export default function ComemonGo() {
         <img src="/images/comemon/logo.png" alt="Logo" className={styles.logo} />
       </div>
       <div className={styles.comemonsContainer}>
-        <Comemons />
-        <Comemons />
-        <Comemons />
-        <Comemons />
-        <Comemons />
+        {data.map((item, index) => (
+          <Comemons key={index} name={item.nickname} level={`Lv. ${item.commit_count}`} commitCount={item.commit_count} />
+        ))}
       </div>
     </div>
   )
--- src/components/analytics/comemon/Comemons.jsx
+++ src/components/analytics/comemon/Comemons.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import styles from '../../../styles/ComemonGo.module.css'
 
-export default function Comemons() {
+export default function Comemons({ name, level, commitCount }) {
+  const getComemonImage = (commitCount) => {
+    if (commitCount <= 10) {
+      return "/images/comemon/1.png";
+    } else if (commitCount <= 25) {
+      return "/images/comemon/2.png";
+    } else if (commitCount <= 35) {
+      return "/images/comemon/3.png";
+    } else if (commitCount <= 60) {
+      return "/images/comemon/4.png";
+    } else if (commitCount <= 80) {
+      return "/images/comemon/4.png";
+    } else if (commitCount <= 130) {
+      return "/images/comemon/5.png";
+    } else {
+      return "/images/comemon/6.png";
+    }
+  }
+
   return (
     <div className={styles.itemContainer}>
       <div className={styles.comemonContainer}>
-        <img src="/images/comemon/6.png" alt="Comemon" className={styles.comemon} />
+        <img src={getComemonImage(commitCount)} alt="Comemon" className={styles.comemon} />
       </div>
       <div className={styles.nameBox}>
         <div className={styles.comemonName}>
-          erica
+          {name}
         </div>
-        <div className={styles.comemonLv}>Lv. 25</div>
+        <div className={styles.comemonLv}>{level}</div>
       </div>
     </div>
   )
