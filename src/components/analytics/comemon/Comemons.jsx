ab35e707c3355b5586f8abd5eb1c96e91c7a9037
moolmin <zzmzzm0065@gmail.com>
2024-07-09 16:46:40 +0900

    save stash

--- src/components/analytics/comemon/Comemons.jsx
+++ src/components/analytics/comemon/Comemons.jsx
@@ -3,17 +3,17 @@ import styles from '../../../styles/ComemonGo.module.css'
 
 export default function Comemons({ name, level, commitCount }) {
   const getComemonImage = (commitCount) => {
-    if (commitCount <= 10) {
+    if (commitCount <= 20) {
       return "/images/comemon/1.png";
-    } else if (commitCount <= 25) {
+    } else if (commitCount <= 40) {
       return "/images/comemon/2.png";
-    } else if (commitCount <= 35) {
-      return "/images/comemon/3.png";
     } else if (commitCount <= 60) {
-      return "/images/comemon/4.png";
+      return "/images/comemon/3.png";
     } else if (commitCount <= 80) {
       return "/images/comemon/4.png";
-    } else if (commitCount <= 130) {
+    } else if (commitCount <= 100) {
+      return "/images/comemon/4.png";
+    } else if (commitCount <= 120) {
       return "/images/comemon/5.png";
     } else {
       return "/images/comemon/6.png";
