5ab3004f1acbeca6436d34114223e273f9578c67
erica0321 <hyunahrho29@gmail.com>
2024-07-09 14:40:11 +0900

    chore: add utils

--- src/utils/static.js
+++ src/utils/static.js
@@ -6,3 +6,26 @@ export const URL = {
   settings: '/settings',
   logOut: '/',
 }
+
+export const GITHUB_LOGIN =
+  'https://github.com/login/oauth/authorize?client_id=Ov23lifYg9wKiqtJvRsK&scope=repo'
+
+export const BACK_TOKEN =
+  'https://api.commitmate-dev.kro.kr/api/auth/github/callback'
+
+export const SERVER_URL = 'https://api.commitmate-dev.kro.kr/api'
+
+export const ORGANIZATION = 'MENTAL-404'
+
+const getAccessToken = () => {
+  return localStorage.getItem('access_token')
+}
+
+export const AUTH_HEADER = {
+  Authorization: `Bearer ${getAccessToken()}`,
+  'Content-Type': 'application/json',
+}
+
+export const getSelectedRepo = () => {
+  return localStorage.getItem('selected_repo')
+}
