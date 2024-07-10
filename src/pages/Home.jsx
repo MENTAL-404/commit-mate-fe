b494a31eb6ae4c63dfe10b879e35df754d3b1d92
erica0321 <hyunahrho29@gmail.com>
2024-07-10 13:27:09 +0900

    chore: modify home styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -89,51 +89,56 @@ export default function Home() {
 
   const truncateNickname = (nickname) => {
     if (Array.isArray(nickname)) {
-      nickname = nickname[0];
+      nickname = nickname[0]
     }
-    return nickname.length > 10 ? `${nickname.substring(0, 10)}..` : nickname;
-  };
+    return nickname.length > 10 ? `${nickname.substring(0, 10)}..` : nickname
+  }
 
   return (
     <Layout>
-      <div className={styles.topContainer}>
+      <div className={styles.main}>
+        <div className={styles.topContainer}>
         📌 커밋메이트는 프로젝트 협업 관리 툴로, 깃허브 활동 상황을 실시간으로
         볼 수 있는 대시보드입니다.
-      </div>
-      <div className={styles.bottomOuterContainer}>
-        <div className={styles.wordCloudContainer}>
+        </div>
+        <div className={styles.bottomOuterContainer}>
+          <div className={styles.wordCloudContainer}>
             <ComemonGo />
-        </div>
-        <div className={styles.middleContainer}>
-          <div className={styles.activeContainer}>
-            <h1 className={styles.title}>활동차트</h1>
-            <div className={styles.activeChart}>
-              <ActiveBarChart />
+          </div>
+          <div className={styles.middleContainer}>
+            <div className={styles.activeContainer}>
+              <h1 className={styles.title}>활동차트</h1>
+              <div className={styles.activeChart}>
+                <ActiveBarChart />
+              </div>
+            </div>
+            <div className={styles.issueContainer}>
+              <h1 className={styles.title}>깃헙 이슈</h1>
+              <div className={styles.issue}>
+                <IssueList className={styles.issue} type='open' />
+              </div>
             </div>
           </div>
-          <div className={styles.issueContainer}>
-            <h1 className={styles.title}>깃헙 이슈</h1>
-            <div className={styles.issue}>
-              <IssueList className={styles.issue} type='open' />
+          <div className={styles.etcContainer}>
+            <h1 className={styles.title}>명예의 전당</h1>
+            <div className={styles.bottomContainer}>
+              <BottomTag
+                image={totalCommitImg}
+                bottom='총 커밋수'
+                title={totalCommit}
+              />
+              <BottomTag
+                image={commitKingImg}
+                bottom='저번주 커밋왕'
+                title={truncateNickname(commitKing)}
+              />
+              <BottomTag
+                image={mergePrImg}
+                bottom='Merge / PR'
+                title={`${mergePr?.merge_count} / ${mergePr?.pr_count}`}
+              />
             </div>
           </div>
-        </div>
-        <div className={styles.bottomContainer}>
-          <BottomTag
-            image={totalCommitImg}
-            bottom='총 커밋수'
-            title={totalCommit}
-          />
-          <BottomTag
-            image={commitKingImg}
-            bottom='저번주 커밋왕'
-            title={truncateNickname(commitKing)}
-          />
-          <BottomTag
-            image={mergePrImg}
-            bottom='Merge / PR'
-            title={`${mergePr?.merge_count} / ${mergePr?.pr_count}`}
-          />
         </div>
       </div>
     </Layout>
