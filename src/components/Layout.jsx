e85781b9482397c93e27b2db668ac4337415afa3
erica0321 <hyunahrho29@gmail.com>
2024-07-11 00:59:07 +0900

    feat: redirect if unauthorized user access

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,16 @@ import SideBar from './SideBar'
 import RightSideBar from './RightSideBar'
 import styles from '../styles/Layout.module.css'
 import RightSideTopContainer from './RightSideTopContainer'
+import { getAccessToken } from '../utils/static'
+import { useNavigate } from 'react-router-dom'
+
 export default function Layout({ children }) {
+  const navigate = useNavigate()
+  const accessToken = getAccessToken()
+  if (!accessToken || accessToken === null || accessToken === '') {
+    navigate('/')
+  }
+
   return (
     <div className={styles.main}>
       <RightSideTopContainer customStyle='topContainerInHome' />
