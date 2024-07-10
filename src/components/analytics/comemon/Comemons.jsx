690976a8e2dba9ac72de028ba8d996b1c374dd68
moolmin <zzmzzm0065@gmail.com>
2024-07-10 20:41:42 +0900

    style: add 7th and 8th cats

--- src/components/analytics/comemon/Comemons.jsx
+++ src/components/analytics/comemon/Comemons.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import styles from '../../../styles/ComemonGo.module.css'
 
 export default function Comemons({ name, level, commitCount }) {
+
   const getComemonImage = (commitCount) => {
-    if (commitCount <= 20) {
+    if (commitCount <= 25) {
       return "../../../images/comemon/1.png";
-    } else if (commitCount <= 40) {
+    } else if (commitCount <= 50) {
       return "../../../images/comemon/2.png";
-    } else if (commitCount <= 60) {
+    } else if (commitCount <= 75) {
       return "../../../images/comemon/3.png";
-    } else if (commitCount <= 80) {
-      return "../../../images/comemon/4.png";
     } else if (commitCount <= 100) {
+      return "../../../images/comemon/4.png";
+    } else if (commitCount <= 125) {
       return "../../../images/comemon/5.png";
-    } else if (commitCount <= 120) {
+    } else if (commitCount <= 150) {
       return "../../../images/comemon/6.png";
-    }
+    } else if (commitCount <= 180) {
+      return "../../../images/comemon/7.png";
+    } return  "../../../images/comemon/8.png";
   }
 
   return (
