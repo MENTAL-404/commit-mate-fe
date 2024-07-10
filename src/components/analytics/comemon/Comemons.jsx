5955b11f6232eefbae1ca242db833be1627b90c4
erica0321 <hyunahrho29@gmail.com>
2024-07-11 01:00:29 +0900

    chore: modify comemons

--- src/components/analytics/comemon/Comemons.jsx
+++ src/components/analytics/comemon/Comemons.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
 import styles from '../../../styles/ComemonGo.module.css'
+import commemon1 from '../../../images/comemon/1.png'
+import commemon2 from '../../../images/comemon/2.png'
+import commemon3 from '../../../images/comemon/3.png'
+import commemon4 from '../../../images/comemon/4.png'
+import commemon5 from '../../../images/comemon/5.png'
+import commemon6 from '../../../images/comemon/6.png'
+import commemon7 from '../../../images/comemon/7.png'
+import commemon8 from '../../../images/comemon/8.png'
 
 export default function Comemons({ name, level, commitCount }) {
-
   const getComemonImage = (commitCount) => {
     if (commitCount <= 25) {
-      return "../../../images/comemon/1.png";
+      return commemon1
     } else if (commitCount <= 50) {
-      return "../../../images/comemon/2.png";
+      return commemon2
     } else if (commitCount <= 75) {
-      return "../../../images/comemon/3.png";
+      return commemon3
     } else if (commitCount <= 100) {
-      return "../../../images/comemon/4.png";
+      return commemon4
     } else if (commitCount <= 125) {
-      return "../../../images/comemon/5.png";
+      return commemon5
     } else if (commitCount <= 150) {
-      return "../../../images/comemon/6.png";
+      return commemon6
     } else if (commitCount <= 180) {
-      return "../../../images/comemon/7.png";
-    } return  "../../../images/comemon/8.png";
+      return commemon7
+    }
+    return commemon8
   }
 
   return (
     <div className={styles.itemContainer}>
       <div className={styles.comemonContainer}>
-        <img src={getComemonImage(commitCount)} alt="Comemon" className={styles.comemon} />
+        <img
+          src={getComemonImage(commitCount)}
+          alt='Comemon'
+          className={styles.comemon}
+        />
       </div>
       <div className={styles.nameBox}>
-        <div className={styles.comemonName}>
-          {name}
-        </div>
+        <div className={styles.comemonName}>{name}</div>
         <div className={styles.comemonLv}>{level}</div>
       </div>
     </div>
