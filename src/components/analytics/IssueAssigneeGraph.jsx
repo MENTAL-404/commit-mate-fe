3520a3b32445185d1938a4bd8464001010170e5e
erica0321 <hyunahrho29@gmail.com>
2024-07-11 00:59:48 +0900

    feat: connect to issue assigneee api

--- src/components/analytics/IssueAssigneeGraph.jsx
+++ src/components/analytics/IssueAssigneeGraph.jsx
@@ -1,42 +1,92 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import issueData from '../../data/issueData.json';
+import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+
+import { useEffect, useState } from 'react'
+
+import {
+  getSelectedRepo,
+  SERVER_URL,
+  ORGANIZATION,
+  getHeader,
+} from '../../utils/static'
 
 const IssueAssigneeGraph = () => {
-  const data = issueData.assignees.data.map(assignee => ({
+  const [issues, setIssues] = useState([])
+
+  useEffect(() => {
+    const fetchAssignee = async () => {
+      try {
+        const response = await fetch(
+          `${SERVER_URL}/organizations/${ORGANIZATION}/repositories/${getSelectedRepo()}/issues/status`,
+          {
+            headers: getHeader(),
+            credentials: 'include',
+          }
+        )
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch issue assignee')
+        }
+        const data = await response.json()
+        setIssues(data.data)
+      } catch (error) {
+        console.error('Error fetching issue assignee:', error)
+      }
+    }
+
+    fetchAssignee()
+  }, [])
+
+  const data = issues.map((assignee) => ({
     name: assignee.nickname,
     open: assignee.open,
-    closed: assignee.closed,
-  }));
+    closed: assignee.close,
+  }))
 
   const CustomTick = ({ x, y, payload }) => {
-    const { value } = payload;
-    const assignee = issueData.assignees.data.find(assignee => assignee.nickname === value);
+    const { value } = payload
+    const assignee = issues.find((assignee) => assignee.nickname === value)
 
     return (
       <g transform={`translate(${x},${y})`}>
         <defs>
           <clipPath id={`clip-circle-${value}`}>
-            <circle cx="0" cy="12" r="12" />
+            <circle cx='0' cy='12' r='12' />
           </clipPath>
         </defs>
         <image
-          href={assignee.profile_image}
+          href={assignee.avatar_url}
           x={-12}
           y={0}
           height={24}
           width={24}
           clipPath={`url(#clip-circle-${value})`}
         />
-        <text x={0} y={35} dy={0} textAnchor="middle" fill="#666" fontSize={14} fontWeight="400">
+        <text
+          x={0}
+          y={35}
+          dy={0}
+          textAnchor='middle'
+          fill='#666'
+          fontSize={14}
+          fontWeight='400'
+        >
           {value}
         </text>
       </g>
-    );
-  };
+    )
+  }
 
   return (
-    <ResponsiveContainer width="100%" height={350}>
+    <ResponsiveContainer width='100%' height={350}>
       <BarChart
         width={500}
         height={200}
@@ -47,22 +97,48 @@ const IssueAssigneeGraph = () => {
           left: 50,
           bottom: 50,
         }}
-        stackOffset="sign"
+        stackOffset='sign'
       >
-        <CartesianGrid strokeDasharray="3 3" vertical={false} horizontal={true}/>
+        <CartesianGrid
+          strokeDasharray='3 3'
+          vertical={false}
+          horizontal={true}
+        />
         <XAxis
-          dataKey="name"
+          dataKey='name'
           tick={<CustomTick />}
           axisLine={true}
           tickLine={false}
         />
         <Tooltip contentStyle={{ fontSize: 12, fontWeight: '500' }} />
-        <Legend verticalAlign="top" align="right" wrapperStyle={{ fontSize: 14, fontWeight: '600', marginBottom: '20px' }} />
-        <Bar dataKey="closed" stackId="a" fill="#EB763C" radius={[0, 0, 0, 0]} name="해결된 이슈" barSize={50} />
-        <Bar dataKey="open" stackId="a" fill="#82ca9d" radius={[0, 0, 0, 0]} name="오픈된 이슈" barSize={50} />
+        <Legend
+          verticalAlign='top'
+          align='right'
+          wrapperStyle={{
+            fontSize: 14,
+            fontWeight: '600',
+            marginBottom: '20px',
+          }}
+        />
+        <Bar
+          dataKey='closed'
+          stackId='a'
+          fill='#EB763C'
+          radius={[0, 0, 0, 0]}
+          name='해결된 이슈'
+          barSize={50}
+        />
+        <Bar
+          dataKey='open'
+          stackId='a'
+          fill='#82ca9d'
+          radius={[0, 0, 0, 0]}
+          name='오픈된 이슈'
+          barSize={50}
+        />
       </BarChart>
     </ResponsiveContainer>
-  );
-};
+  )
+}
 
-export default IssueAssigneeGraph;
+export default IssueAssigneeGraph
