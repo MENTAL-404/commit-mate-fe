947943ddb2125fd9535007f2d03affe744207435
erica0321 <hyunahrho29@gmail.com>
2024-07-11 19:31:07 +0900

    chore: modify LoadingLottie component

--- src/components/LoadingLottie.jsx
+++ src/components/LoadingLottie.jsx
@@ -3,9 +3,9 @@ import loadingIndicator from '../images/loading 5.json'
 import styles from '../../src/styles/Loading.module.css'
 import React from 'react'
 
-export default function LoadingLottie() {
+export default function LoadingLottie({ width }) {
   return (
-    <div className={styles.loadingContainer}>
+    <div className={styles.loadingContainer} style={{ width: `${width}` }}>
       <Lottie animationData={loadingIndicator} />
     </div>
   )
