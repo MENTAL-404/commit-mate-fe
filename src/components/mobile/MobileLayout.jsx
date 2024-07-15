ce110d29db7f9707c13eab81c616df6c1693e814
erica0321 <hyunahrho29@gmail.com>
2024-07-15 18:33:16 +0900

    chore: modify mobile layout

--- src/components/mobile/MobileLayout.jsx
+++ src/components/mobile/MobileLayout.jsx
@@ -1,16 +1,14 @@
-import styles from '../../styles/MobileLayout.module.css';
-import React from 'react';
-import MobileHeader from './MobileHeader';
-import MobileFooter from './MobileFooter';
+import styles from '../../styles/MobileLayout.module.css'
+import React from 'react'
+import MobileHeader from './MobileHeader'
+import MobileFooter from './MobileFooter'
 
 export default function MobileLayout({ children }) {
   return (
     <div className={styles.container}>
       <MobileHeader />
-      <main className={styles.mainContent}>
-        {children}
-      </main>
+      <main className={styles.mainContent}>{children}</main>
       <MobileFooter />
     </div>
-  );
+  )
 }
