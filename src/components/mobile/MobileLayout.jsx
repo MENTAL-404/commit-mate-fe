9aa26ec281ce18f7ae38758f479662c4d045071f
erica0321 <hyunahrho29@gmail.com>
2024-07-16 00:09:28 +0900

    feat: add login page

--- src/components/mobile/MobileLayout.jsx
+++ src/components/mobile/MobileLayout.jsx
@@ -2,13 +2,17 @@ import styles from '../../styles/mobile/MobileLayout.module.css'
 import React from 'react'
 import MobileHeader from './MobileHeader'
 import MobileFooter from './MobileFooter'
+import { useLocation } from 'react-router-dom'
 
 export default function MobileLayout({ children }) {
+  const location = useLocation()
+  const path = location.pathname
+
   return (
     <div className={styles.container}>
       <MobileHeader />
       <main className={styles.mainContent}>{children}</main>
-      <MobileFooter />
+      {path === '/' || <MobileFooter />}
     </div>
   )
 }
