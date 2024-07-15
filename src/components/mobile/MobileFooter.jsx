8461e354485dfad99bf54453d395b3127d32f5ea
erica0321 <hyunahrho29@gmail.com>
2024-07-15 17:11:38 +0900

    add: changing icon color

--- src/components/mobile/MobileFooter.jsx
+++ src/components/mobile/MobileFooter.jsx
@@ -3,26 +3,60 @@ import styles from '../../styles/MobileFooter.module.css'
 import issue from '../../assets/images/sidebar/issue.png'
 import setting from '../../assets/images/sidebar/setting.png'
 import homeOrange from '../../assets/images/sidebar/homeOrange.png'
+import home from '../../assets/images/sidebar/home.png'
 import commit from '../../assets/images/sidebar/commit.png'
+import commitOrange from '../../assets/images/sidebar/commitOrange.png'
+import settingOrange from '../../assets/images/sidebar/settingOrange.png'
+import issueOrange from '../../assets/images/sidebar/issueOrange.png'
+import { Link, useLocation } from 'react-router-dom'
+import { URL } from '../../utils/static'
 
 const MobileFooter = () => {
+  const location = useLocation()
+  const path = location.pathname
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.tab}>
-        <img src={homeOrange} className={styles.icon} />홈
-      </div>
-      <div className={styles.tab}>
-        <img src={commit} className={styles.icon} />
+      <Link
+        to={URL.home}
+        className={`${styles.tab} ${path.includes('home') && styles.clicked}`}
+      >
+        <img
+          src={path.includes('home') ? homeOrange : home}
+          className={styles.icon}
+        />
+        홈
+      </Link>
+      <Link
+        to={URL.commits}
+        className={`${styles.tab} ${path.includes('commit') && styles.clicked}`}
+      >
+        <img
+          src={path.includes('commit') ? commitOrange : commit}
+          className={styles.icon}
+        />
         커밋
-      </div>
-      <div className={styles.tab}>
-        <img src={issue} className={styles.icon} />
+      </Link>
+      <Link
+        to={URL.issues}
+        className={`${styles.tab} ${path.includes('issue') && styles.clicked}`}
+      >
+        <img
+          src={path.includes('issue') ? issueOrange : issue}
+          className={styles.icon}
+        />
         이슈
-      </div>
-      <div className={styles.tab}>
-        <img src={setting} className={styles.icon} />
+      </Link>
+      <Link
+        to={URL.settings}
+        className={`${styles.tab} ${path.includes('setting') && styles.clicked}`}
+      >
+        <img
+          src={path.includes('setting') ? settingOrange : setting}
+          className={styles.icon}
+        />
         설정
-      </div>
+      </Link>
     </footer>
   )
 }
