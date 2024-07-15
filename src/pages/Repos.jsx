2fe1b215f82b6bb8609adf09d87d3d2921d916d5
moolmin <zzmzzm0065@gmail.com>
2024-07-15 11:54:31 +0900

    save stash

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useMemo } from 'react'
 import { toast } from 'react-toastify'
 import ToastMessage from '../components/ToastMessage'
 import 'react-toastify/dist/ReactToastify.css'
-import { ORGANIZATION, getSelectedRepo, API_URL, getSelectedOrg } from '../utils/static'
+import { getSelectedRepo, API_URL, getSelectedOrg } from '../utils/static'
 import useFetchData from '../hooks/useFetchData'
 import LoadingLottie from '../components/LoadingLottie'
 
@@ -15,31 +15,29 @@ export default function Repos() {
 
   const {
     loading: loadingOrg,
-    response, orgResponse,
-    error: errorOrg
+    response: orgResponse,
   } = useFetchData(API_URL().organization_select)
 
   const {
     loading: loadingRepo,
     response: repoResponse,
-    error: errorRepo  }
-    = useFetchData(API_URL().repositories)
+    error: errorRepo
+  } = useFetchData(API_URL().repositories)
 
-
-//조직 선택 ================>
+  // 조직 선택 ================>
   const organizations = useMemo(
     () => (orgResponse ? orgResponse.data.repos : []),
-    [response]
+    [orgResponse]
   )
 
   useEffect(() => {
     setSelectedOrg(getSelectedOrg() || '')
     console.log('orga:', organizations);
-  }, [])
+  }, [organizations])
 
   useEffect(() => {
     if (organizations.length > 0 && !selectedOrg) {
-      setSelectedOrg(repositories[0])
+      setSelectedOrg(organizations[0])
     }
   }, [organizations, selectedOrg])
 
@@ -52,18 +50,16 @@ export default function Repos() {
     toast.success('선택한 조직이 저장되었습니다,')
   }
 
-
-  //레포 선택 ================>
-  // 밑에꺼 수정 ㄱㄱ
+  // 레포 선택 ================>
   const repositories = useMemo(
     () => (repoResponse ? repoResponse.data.repos : []),
-    [response]
+    [repoResponse]
   )
 
   useEffect(() => {
     setSelectedRepo(getSelectedRepo() || '')
     console.log('repo:', repositories);
-  }, [])
+  }, [repositories])
 
   useEffect(() => {
     if (repositories.length > 0 && !selectedRepo) {
@@ -95,37 +91,33 @@ export default function Repos() {
         <h1 className={styles.title}>커밋메이트에 오신 것을 환영합니다.</h1>
         <span>아래에서 작업할 조직과 레포지토리를 선택해주세요.</span>
         <div className={styles.reposContainer}>
-          {/*<div className={styles.nameGroup}>*/}
-          {/*<label className={styles.inputLabel}>조직명</label>*/}
-          {/*<div className={styles.name}>{ORGANIZATION}</div>*/}
-          {/*</div>*/}
           <div className={styles.selectGroup}>
             <label className={styles.inputLabel}>조직</label>
-            {/*{loadingOrg ? (*/}
-            {/*  <LoadingLottie width={'30px'} />*/}
-            {/*) : (*/}
-            {/*  <>*/}
-            {/*    <select*/}
-            {/*      className={styles.select}*/}
-            {/*      value={selectedOrg || ''}*/}
-            {/*      onChange={(e) => handleChangeOrg(e.target.value)}*/}
-            {/*    >*/}
-            {/*      {organizations.map((org) => {*/}
-            {/*        return (*/}
-            {/*          <option value={org} key={org}>*/}
-            {/*            {org}*/}
-            {/*          </option>*/}
-            {/*        )*/}
-            {/*      })}*/}
-            {/*    </select>*/}
-            {/*    <button*/}
-            {/*      className={styles.saveButton}*/}
-            {/*      onClick={handleClickSaveOrg}*/}
-            {/*    >*/}
-            {/*      저장*/}
-            {/*    </button>*/}
-            {/*  </>*/}
-            {/*)}*/}
+            {loadingOrg ? (
+              <LoadingLottie width={'30px'} />
+            ) : (
+              <>
+                <select
+                  className={styles.select}
+                  value={selectedOrg || ''}
+                  onChange={(e) => handleChangeOrg(e.target.value)}
+                >
+                  {organizations.map((org) => {
+                    return (
+                      <option value={org} key={org}>
+                        {org}
+                      </option>
+                    )
+                  })}
+                </select>
+                <button
+                  className={styles.saveButton}
+                  onClick={handleClickSaveOrg}
+                >
+                  저장
+                </button>
+              </>
+            )}
           </div>
           <div className={styles.inputGroup}>
             <div className={styles.selectGroup}>
