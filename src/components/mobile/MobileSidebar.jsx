9b1b089ff46ca4695da215c443b15bfc624b0dcf
moolmin <zzmzzm0065@gmail.com>
2024-07-15 19:46:55 +0900

    chore: change color of sidebar on mobile

--- src/components/mobile/MobileSidebar.jsx
+++ src/components/mobile/MobileSidebar.jsx
@@ -6,8 +6,23 @@ const MobileSidebar = ({ isOpen, onClose }) => {
     <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
       <div className={styles.overlay} onClick={onClose}></div>
       <div className={styles.sidebarContent}>
-        <button className={styles.closeButton} onClick={onClose}></button>
+        <div className={styles.profileSection}>
+          <div className={styles.profileDetails}>
+            <div className={styles.profileImage}> </div>
+            <div>
+              <div className={styles.profileName}>commit-mate-fe</div>
+              <div className={styles.profileChange}>레포지토리 변경</div>
+            </div>
+          </div>
+          <button className={styles.logoutButton}>로그아웃</button>
+        </div>
         {/* Add your sidebar content here */}
+        <div className={styles.menuItem}>환경설정</div>
+        <div className={styles.menuItem}>할 일 목록</div>
+        <hr />
+        <div className={styles.menuItem}>홈</div>
+        <div className={styles.menuItem}>커밋</div>
+        <div className={styles.menuItem}>이슈</div>
       </div>
     </div>
   );
